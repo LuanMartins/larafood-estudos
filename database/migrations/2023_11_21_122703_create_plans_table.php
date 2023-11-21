538c6f2edbf99f43d6df2f5822e68749b97e5626
LuanMartins <luan_18martins@hotmail.com>
2023-11-21 12:59:50 -0300

    refactor: add description column for plans table

--- database/migrations/2023_11_21_122703_create_plans_table.php
+++ database/migrations/2023_11_21_122703_create_plans_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('url');
             $table->decimal('price');
+            $table->longText('description')->nullable();
             $table->timestamps();
         });
     }
