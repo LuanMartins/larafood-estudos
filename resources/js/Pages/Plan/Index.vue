c107d45b1befda4fe25b0aaa9f625e0d5997d609
LuanMartins <luan_18martins@hotmail.com>
2023-11-24 18:20:41 -0300

    fear: create layout with index plans

---
+++ resources/js/Pages/Plan/Index.vue
@@ -0,0 +1,33 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+
+defineProps({plans: Object});
+</script>
+<template>
+    <Head title="Plans" />
+    <AuthenticatedLayout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+                <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                    <table class="table-auto">
+                        <thead>
+                            <tr>
+                            <th>Name</th>
+                            <th>Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in plans.data">
+                                <td>{{ item.name }}</td>
+                                <td>{{ item.price }}</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
