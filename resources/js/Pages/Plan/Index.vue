3e1f19dccdfc36400fd8ab0ce8a1d8bcd2ec009b
LuanMartins <luan_18martins@hotmail.com>
2023-12-28 11:43:02 -0300

    refactor: adjust table columns fixed size

--- resources/js/Pages/Plan/Index.vue
+++ resources/js/Pages/Plan/Index.vue
@@ -37,14 +37,18 @@ watchEffect(() => {
                     <table class="w-full border-collapse border">
                         <thead class="bg-gray-900 text-white">
                             <tr>
-                            <th class="px-2 py-1 text-sm font-bold text-left border-r border-b border-gray-700"><span>Name &duarr;</span></th>
-                            <th class="px-2 py-1 text-sm font-bold text-left border-r border-b border-gray-700"><span>Price &duarr;</span></th>
+                                <th class="w-1/2 px-2 py-1 text-sm font-bold text-left border-r border-b border-gray-700">
+                                    <span>Name &duarr;</span>
+                                </th>
+                                <th class="w-1/2 px-2 py-1 text-sm font-bold text-left border-r border-b border-gray-700">
+                                    <span>Price &duarr;</span>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="item in plans.data">
-                                <td class="px-2 py-1 text-sm text-left">{{ item.name }}</td>
-                                <td class="px-2 py-1 text-sm text-left">{{ formatCurrency(item.price) }}</td>
+                            <tr v-for="item in plans.data" :key="item.id">
+                                <td class="w-1/2 px-2 py-1 text-sm text-left">{{ item.name }}</td>
+                                <td class="w-1/2 px-2 py-1 text-sm text-left">{{ formatCurrency(item.price) }}</td>
                             </tr>
                         </tbody>
                     </table>
